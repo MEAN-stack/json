885a7f6a98e9b2d45ab4f29a5cbcf66d9f25494f
Paul Robertson <paul.robertson@soft-machine.co.uk>
2018-04-08 18:02:56 +0100

    added parser for Reals

--- json.hs
+++ json.hs
@@ -4,6 +4,8 @@ import Control.Applicative
 import Control.Monad
 
 -- JSON library
+
+-- example JavaScript object:
 {--
 {
     "type" : "one",
@@ -11,22 +13,33 @@ import Control.Monad
     "data" :
     {
         "errorCode" : 99.9e6,
-        "message" : "Error"
+        "message" : "Error",
+        "nested" :
+        {
+            "errorCode2" : -99.9e-6,
+            message2 : "Error"
+        }
     },
-    "people":
+    "people" :
     [
         "Paul",
         "Ann",
-        "Joe"
+        "Joe",
+        {
+            errorCode2 : 9.9E6,
+            message2 : "Error"
+        }
     ],
     "status" :
     {
         "age" : 56,
-        "married":true
+        "married" : true
     }
 }
 --}
 
+-- Declare a Haskell data type for JSON value 
+--
 data JValue =
     JNull 
     | JString String
@@ -38,10 +51,19 @@ data JValue =
     | JObject [(String, JValue)]
     deriving (Read, Show)
 
+-- a JSON object is a lisk of key-value pairs
+--
 type Object = [(String, JValue)]
 
-j = JObject [("type", JString "one"), ("id", JReal 1.23), ("data", JObject [("errorCode", JReal 99.9e6), ("message", JString "Error"), ("nested", JObject [("errorCode2", JReal 99.9e6), ("message2", JString "Error")])]), ("people", JArray [JString "Paul", JString "Ann", JString "Joe", JObject [("errorCode2", JReal 99.9e6), ("message2", JString "Error")]]), ("status", JObject [("age", JInt 56), ("married", JTrue)])]
+-- construct a sample JSON value
+-- make it the same as the example JavaScript object above
+--
+j = JObject [("type", JString "one"), ("id", JReal 1.23), ("data", JObject [("errorCode", JReal 99.9e6), ("message", JString "Error"), ("nested", JObject [("errorCode2", JReal (-99.9e-6)), ("message2", JString "Error")])]), ("people", JArray [JString "Paul", JString "Ann", JString "Joe", JObject [("errorCode2", JReal 9.9E6), ("message2", JString "Error")]]), ("status", JObject [("age", JInt 56), ("married", JTrue)])]
 
+-- convert a Haskell JValue to a string
+--
+-- stringify produces a compact single-line output
+--
 stringify :: JValue -> String
 stringify JNull = "null"
 stringify (JString s) = show s
@@ -52,27 +74,26 @@ stringify JFalse = "false"
 stringify (JArray vs) = "[" ++ (intercalate "," $ map stringify vs) ++ "]"
 stringify (JObject os) = "{" ++ (intercalate "," $ map (\(key, val) -> show key ++ ":" ++ stringify val) os) ++ "}"
 
-prettify :: String -> JValue -> String
-prettify spacer (JArray vs)  = "\n" ++ spacer ++ "[\n" ++ (intercalate ",\n" $ map (prettify sp) vs) ++ "\n" ++ spacer ++ "]"
+-- prettify produces a multi-line, indented output
+--
+prettify' :: String -> JValue -> String
+prettify' spacer (JArray vs)  = "\n" ++ spacer ++ "[\n" ++ (intercalate ",\n" $ map (prettify' sp) vs) ++ "\n" ++ spacer ++ "]"
     where sp = spacer ++ "    "
 
-prettify spacer (JObject os) = "\n" ++ spacer ++ "{\n" ++ sp ++ (intercalate (",\n" ++ sp) $ map (\(key, val) -> show key ++ " : " ++ rend sp val) os) ++ "\n" ++ spacer ++ "}"
+prettify' spacer (JObject os) = "\n" ++ spacer ++ "{\n" ++ sp ++ (intercalate (",\n" ++ sp) $ map (\(key, val) -> show key ++ " : " ++ rend sp val) os) ++ "\n" ++ spacer ++ "}"
     where sp = spacer ++ "    "
-          rend sp (JArray vs)  = prettify sp (JArray vs)
-          rend sp (JObject os) = prettify sp (JObject os)
+          rend sp (JArray vs)  = prettify' sp (JArray vs)
+          rend sp (JObject os) = prettify' sp (JObject os)
           rend sp x            = stringify x
 
-prettify spacer x = spacer ++ stringify x
+prettify' spacer x = spacer ++ stringify x
 
--- main = putStrLn $ prettify "" j
--- findJValue "status" j >>= findJValue "age" >>= getInt
--- do
---      status <- findJValue "status" j
---      age <- findJValue "age" status
---      i <- getInt age
---      return i
-        
+prettify :: JValue -> String
+prettify = prettify' ""
 
+{--
+-- These helpers don't seem very helpful
+--
 isNull :: JValue -> Bool
 isNull JNull = True
 isNull _ = False
@@ -104,7 +125,11 @@ isArray _ = False
 isObject :: JValue -> Bool
 isObject (JObject v) = True
 isObject _ = False
+--}
 
+-- Manipulate JSON values
+-- 
+-- 
 getString :: JValue -> Maybe String
 getString (JString s) = Just s
 getString _ = Nothing
@@ -129,53 +154,98 @@ findObjVal :: String -> Object -> Maybe JValue
 findObjVal key [] = Nothing
 findObjVal key ((k,v):vs) = if key == k then Just v else findObjVal key vs
 
+-- TODO
+-- stuff like this (JavaScript):
+-- console.log(j.data.nested.message2)
+-- j.people[2] = "Will"
+-- j.people.push(100)
+-- delete j.status.age
 
--- test JSON to parse
--- {"type":"one","id":1.23,"data":{"errorCode":9.99e7,"message":"Error","nested":{"errorCode2":9.99e7,"message2":"Error"}},"people":["Paul","Ann","Joe",{"errorCode2":9.99e7,"message2":"Error"}],"status":{"age":56,"married":true}}
+-- Parsing
+--
+-- Whitespace consumer
+--
+ws :: Parser String
+ws = many (oneOf " \t\r\n")
 
-                
-jsonTrue' :: Parser JValue
-jsonTrue' = (string "true") *> (pure JTrue)
+lexeme :: Parser a -> Parser a
+lexeme p = p <* ws
+
+char' = lexeme . char
+
+-- simple values
+--
+jsonTrue :: Parser JValue
+jsonTrue = lexeme $ (string "true") *> (pure JTrue)
 
 jsonFalse :: Parser JValue
-jsonFalse = (string "false") *> (pure JFalse)
+jsonFalse = lexeme $ (string "false") *> (pure JFalse)
 
 jsonNull :: Parser JValue
-jsonNull = (string "null") *> (pure JNull)
+jsonNull = lexeme $ (string "null") *> (pure JNull)
 
+-- strings
+--
 jstring :: Parser String
 jstring = char '"' *> (many (noneOf ['"'])) <* char '"'
 
-jint :: Parser String
-jint = many1 (oneOf "0123456789")
+jsonString :: Parser JValue
+jsonString = lexeme $ fmap JString jstring
 
-jsonInt :: Parser JValue
-jsonInt = do
-            sign <- jsign
-            num <- jint
-            if ( sign == '-' ) then
-                return (JInt ((-1) * (read num :: Int))) 
-            else 
-                if ( sign == '+' ) then
-                    return (JInt (read num :: Int))
-                else
-                    return (JInt (read (sign:num) :: Int))
-
+-- signed integers
+--
 jsign :: Parser Char
 jsign = (char '+') <|> (char '-') <|> (oneOf "0123456789")
 
+jint :: Parser String
+jint = many (oneOf "0123456789")
 
+jsonInt' :: Parser Int
+jsonInt' = do
+                sign <- jsign
+                num <- jint
+                if ( sign == '-' ) then
+                    return ((-1) * (read num :: Int)) 
+                else 
+                    if ( sign == '+' ) then
+                        return (read num :: Int)
+                    else
+                        return (read (sign:num) :: Int)
 
+jsonInt :: Parser JValue
+jsonInt = fmap JInt jsonInt'
 
-jsonString' :: Parser JValue
-jsonString' = fmap JString jstring
+-- real numbers
+--
+jsonRealExp' :: Parser Double
+jsonRealExp' = do
+                ip <- jsonInt'
+                char '.'
+                dp <- jint
+                oneOf "eE"
+                exp <- jsonInt'
+                return  ( (10 ** (fromIntegral exp) ) * (fromIntegral $ signum ip ) * ((fromIntegral $ abs ip) + read ("0." ++ dp) :: Double))
 
+jsonReal' :: Parser Double
+jsonReal' = do
+                ip <- jsonInt'
+                char '.'
+                dp <- jint
+                return  ( (fromIntegral $ signum ip ) * ((fromIntegral $ abs ip) + read ("0." ++ dp) :: Double))
+
+jsonReal :: Parser JValue
+jsonReal = fmap JReal (try jsonRealExp' <|> jsonReal')
+
+-- arrays
+--
 jarray :: Parser [JValue]
 jarray = char' '[' *> (jsonValue `sepBy` (char' ',')) <* char ']'
 
 jsonArray :: Parser JValue
 jsonArray = fmap JArray $ lexeme jarray
 
+-- objects
+--
 keyValue :: Parser (String, JValue)
 keyValue  = do
                key <- jstring
@@ -188,18 +258,11 @@ jsonObject :: Parser JValue
 jsonObject = JObject <$> (char' '{' *> (keyValue `sepBy` (char' ',')) <* char '}')
 
 jsonValue' :: Parser JValue
-jsonValue' = jsonTrue <|> jsonFalse <|> jsonNull <|> jsonString <|> jsonArray <|> jsonObject
+jsonValue' = jsonTrue <|> jsonFalse <|> jsonNull <|> jsonString <|> try jsonReal <|> jsonInt <|> jsonArray <|> jsonObject
 
 jsonValue = ws *> jsonValue'
 
-ws :: Parser String
-ws = many (oneOf " \t\r\n")
 
---lexeme :: Parser a -> Parser a
-lexeme p = p <* ws
+s = stringify j
 
-char' = lexeme . char
-
-jsonTrue = lexeme jsonTrue'
-
-jsonString = lexeme jsonString'
+jv = parseTest jsonValue s
